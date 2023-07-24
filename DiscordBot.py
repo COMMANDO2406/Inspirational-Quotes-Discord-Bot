d68ba8b143296863b83ef6b33144aa42c87c8b79
COMMANDO2406 <raghavsethi.in@gmail.com>
2023-07-24 10:31:44 +0530

    changed logic for execution

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,71 +1,33 @@
-import requests
-from PIL import Image, ImageDraw, ImageFont
-import random
 import discord
 from discord.ext import commands
+import subprocess
 
-BOT_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
+BOT_TOKEN = "BOT TOKEN"
 # If you want to test out this bot you can read BOT_SETUP.md for instructions on how to run this
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
-@bot.command()
-async def inspire(ctx):
-    # get quote
-    URL = "https://api.goprogram.ai/inspiration"
-    r = requests.get(URL)
-    x = r.json()
+@bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):  # Check if the bot is mentioned in the message
+        await inspire(message)  # Call the inspire command when the bot is mentioned
 
-    quote = x["quote"]
-    author = x["author"]
+    await bot.process_commands(message)  # Process other commands if any
 
-    # get image
-    x = (random.randint(1, 11))
-    img_path = "Background-Images/{imagename}.jpg"
-    img = Image.open(img_path.format(imagename=x))
-
-    draw = ImageDraw.Draw(img)
-    width, height = img.size
-
-    fontsize = 30
-    font = ImageFont.truetype("arial.ttf", fontsize)
-
-    max_width = int(0.8 * width)
-
-    # quote text-box positional logic
-    quote_bbox = draw.textbbox((0, 0), quote, font=font)
-    quote_width = quote_bbox[2] - quote_bbox[0]
-    quote_height = quote_bbox[3] - quote_bbox[1]
-    quote_x = (width - quote_width) // 2
-    quote_y = (height - quote_height) // 2
-
-    # author text-box positional logic
-    author_bbox = draw.textbbox((0, 0), author, font=font)
-    author_width = author_bbox[2] - author_bbox[0]
-    author_height = author_bbox[3] - author_bbox[1]
-    author_x = (width - author_width) // 2
-    author_y = quote_y + author_height + 20
-
-    # draw onto the image
-    draw.text((quote_x, quote_y), quote, font=font, fill='white')
-    draw.text((author_x, author_y), "- " + author, font=font, fill='white')
-
-    # Save the image to a file
-    img_path = 'Output.png'
-    img.save(img_path, format="PNG")
+async def inspire(message):
+    # Execute main.py to generate the quote and image
+    subprocess.run(['python', 'main.py'])
 
     # Send the image as a file on Discord
-    with open(img_path, 'rb') as f:
+    with open('Output.png', 'rb') as f:
         image_file = discord.File(f)
-        await ctx.send(f'"{quote}" - {author}', file=image_file)
-
-    img.show()
+        await message.channel.send(file=image_file)
 
 bot.run(BOT_TOKEN)
